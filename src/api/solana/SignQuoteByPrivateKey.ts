a0e3d7c18d6bd61fbe63734aba8d4e6d59bcf45b
Sai <kldtks@live.com>
2024-06-24 11:53:52 +0800

    new api solana sign

--- src/api/solana/SignQuoteByPrivateKey.ts
+++ src/api/solana/SignQuoteByPrivateKey.ts
@@ -1,25 +1,21 @@
-import { ethers } from "ethers";
 import { _getSignDataEIP712 } from "../../business/evm";
 import { Quote, SignData } from "../../interface/interface";
 import { mathReceived } from "../../utils/math";
+import { decodeUTF8 } from 'tweetnacl-util';
+import * as nacl from 'tweetnacl';
 
-export const _signQuoteEIP712ByPrivateKey =     
+export const _signQuoteByPrivateKey = 
     (quote: Quote, privateKey: string, network: string, amount: string, swapToNative: number, receivingAddress: string,
     stepTimeLock: number | undefined, rpcSrc: string | undefined, rpcDst: string | undefined) => 
-        new Promise<{signData: SignData, signed: string}>(async (resolve, reject) => {
-            
+        new Promise<void>(async (resolve, reject) => {
+        
     const {dstAmount, dstNativeAmount} = mathReceived(quote, amount, swapToNative)
     
     const signData: SignData = await _getSignDataEIP712(quote, network, amount, dstAmount, dstNativeAmount, swapToNative, receivingAddress, stepTimeLock, rpcSrc, rpcDst)
 
-    const web3Wallet = new ethers.Wallet(privateKey)
-
-    signData.message.requestor = web3Wallet.address
-
-    const signed = await web3Wallet.signTypedData(signData.domain, signData.types, signData.message)
-
-    resolve({
-        signData,
-        signed
-    })
 })
+
+
+
+
+

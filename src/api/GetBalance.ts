9493ef8d6f9d4b4270c9d2c2c1f7bce42c1d3524
Tang Wei <tw7613781@gmail.com>
2024-06-26 23:53:40 +0800

    feat: add GetBalance api for solana

--- src/api/GetBalance.ts
+++ src/api/GetBalance.ts
@@ -1,4 +1,5 @@
 import { getBalance as getBalanceEVM } from "../business/evm";
+import { getBalance as getBalanceSOLANA } from "../business/solana";
 import { Bridge } from "../interface/interface";
 import { getChainType } from "../utils/chain";
 
@@ -11,6 +12,9 @@ export const getBalance = (bridge: Bridge, address: string, network: string,
         case 'evm':
             resolve(await getBalanceEVM(network, bridge.src_chain_id, bridge.src_token, address, rpc))
             return 
+        case 'solana':
+            resolve(await getBalanceSOLANA(network, bridge.src_chain_id, bridge.src_token, address, rpc))
+            return
         default:
             throw new Error(`not support chain: ${bridge.src_chain_id}`);
             

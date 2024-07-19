4a390eb07df365bdf3aa1fc2f63bbef566594f28
Tang Wei <tw7613781@gmail.com>
2024-07-19 11:53:10 +0800

    fix: a typo at GetBusiness example

--- example/GetBusiness.ts
+++ example/GetBusiness.ts
@@ -1,4 +1,4 @@
-import { Business, Relay } from '../src/index'
+import { Business, BusinessFullData, Relay } from '../src/index'
 
 const RELA_URL = 'https://5b4522f4.vaughnmedellins394.myterminus.com'
 const HASH = '0xc3938b65e57a479b4622467e3c43f7f59d54f467a44793e9e2fb69f7076348a3'
@@ -7,7 +7,7 @@ const GetBusiness = async () => {
     const business: Business = await new Relay(RELA_URL).getBusiness(HASH)
     console.log('business', business)
 
-    const businessFull: Business = await new Relay(RELA_URL).getBusinessFull(HASH)
+    const businessFull: BusinessFullData = await new Relay(RELA_URL).getBusinessFull(HASH)
     console.log('full business', businessFull)
 }
 

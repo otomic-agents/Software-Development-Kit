c6b9a504f24a35d6f727ee52a84b62d53b64a996
Tang Wei <tw7613781@gmail.com>
2024-09-12 13:01:01 +0800

    fix: make a manual reconnection mechanism in socket io for relay server connection

--- src/api/Quote.ts
+++ src/api/Quote.ts
@@ -1,14 +1,17 @@
-import { Socket, io } from "socket.io-client";
+import { Socket, io } from "socket.io-client"
 
-import { AskIF } from "../interface/interface";
-import { OnQuoteIF } from "../interface/interface";
-import { jsonParser } from "../utils/data";
-
+import { AskIF } from "../interface/interface"
+import { OnQuoteIF } from "../interface/interface"
+import { jsonParser } from "../utils/data"
+import { sleep } from "../utils/sleep"
 export class QuoteManager {
-    
-    asking: boolean = false
 
+    asking: boolean = false
     socket: Socket | undefined = undefined
+    reconnectionAttempts: number = 0
+    maxReconnectAttempts: number = 8
+    reconnectInterval: number = 1000
+    gotQuote: boolean = false
 
     ask = (askIF: AskIF, callback: OnQuoteIF, relayUrl: string, timeout: number = 1000 * 30) => {
 
@@ -17,40 +20,64 @@ export class QuoteManager {
         }
 
         this.asking = true
-
-        this.socket = io(`${relayUrl}`, {
-            withCredentials: true,
-            reconnection: true,
-            reconnectionAttempts: 5,
-        })
+        this.socket = io(`${relayUrl}`, {transports: ["websocket"]})
 
         let connectionTimeout = setTimeout(() => {
             throw new Error('connection timed out')
-          }, timeout);
+        }, timeout);
 
-        this.socket.on("quote", data => {          
+        this.socket.on("quote", data => {
             callback.OnQuote(jsonParser(data))
+            this.gotQuote = true
         })
 
         this.socket.on("connect", () => {
-            const bridgeName = `${askIF.bridge.src_chain_id}_${askIF.bridge.dst_chain_id}_${askIF.bridge.src_token}_${askIF.bridge.dst_token}`
-            this.socket?.emit("ask", bridgeName, askIF.amount);
-            clearTimeout(connectionTimeout)
+            if (!this.gotQuote) {
+                const bridgeName = `${askIF.bridge.src_chain_id}_${askIF.bridge.dst_chain_id}_${askIF.bridge.src_token}_${askIF.bridge.dst_token}`
+                this.socket?.emit("ask", bridgeName, askIF.amount)
+                console.log('Sent a ask quote message')
+                clearTimeout(connectionTimeout)
+            }
+            console.log('Connected to server')
+            this.reconnectionAttempts = 0
+            
         })
 
-        this.socket.on("disconnect", () => {
+        this.socket.on("disconnect", async (reason) => {
             this.asking = false
             clearTimeout(connectionTimeout)
-        })
+            console.log('Disconnected:', reason)
+            await this.tryReconnect()
+        })  
 
-        this.socket.on('connect_error', (error: any) => {
+        this.socket.on('connect_error', async (error: any) => {
             clearTimeout(connectionTimeout)
-            throw new Error(error)
+            console.error('connect error:', error)
+            await this.tryReconnect()
         });
 
-        this.socket.on('reconnect', (attemptNumber) => {
-            console.log('Reconnected after', attemptNumber, 'attempts');
+        this.socket.on('error', async (error: any) => {
+            clearTimeout(connectionTimeout)
+            console.error('error:', error); 
+            await this.tryReconnect()
         });
+    }
+
+    tryReconnect = async () => {
+        if (this.socket) {
+            this.socket.close();
+            this.reconnectionAttempts += 1;
+            console.log('Reconnecting', this.reconnectionAttempts);
+            if (this.reconnectionAttempts <= this.maxReconnectAttempts) {
+                let waitTime = this.reconnectInterval * Math.pow(2, this.reconnectionAttempts)
+                console.log('Waiting', waitTime, 'ms before reconnecting')
+                await sleep(waitTime)
+                this.socket.connect()
+            } else {
+                console.log('Max reconnection attempts reached')
+                throw new Error('failed to connect relay server')
+            }
+        }
     }
 
     stopAsk = () => {
